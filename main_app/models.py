9881195857948f9b6a85530ef0e86bcb2544ac5a
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2021-11-28 10:52:03 -0500

    added model and index view

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Info(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Info
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'latest_Info_list'
+
+    def get_queryset(self):
+        
+        return Info.objects.order_by('updated_on')[:5]
